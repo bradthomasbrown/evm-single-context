15e6cc2a33e616640cbe78f1f092728095e11568
Brad Brown <bradthomasbrown@outlook.com>
2026-01-19 21:27:45 -0600

    need to actually copy returndata deployed byte cost is handled elsewhere value transfer handled elsewhere add some necessary subcontext information state diff stack push handled elsewhere

--- src/opsets/9db704/opcodes/CALL.ts
+++ src/opsets/9db704/opcodes/CALL.ts
@@ -49,17 +49,15 @@ export default {
                 context.pc++;
             } else {
                 if (context.subcontext!.reverted === null) return;
-                const [_gas, _address, _value, _argsOffset, _argsSize, _retOffset, _retSize] = pop(context.stack, 7);
-                // if (context.states.at(-1)!.accounts === null) context.states.at(-1)!.accounts = new Map();
+                const [_gas, _address, _value, _argsOffset, _argsSize, retOffset, retSize] = pop(context.stack, 7);
+                copy(context.memory!, context.subcontext!.returndata, Number(retOffset), 0, Number(retSize), false);
                 context.stack.push(context.subcontext!.reverted === false ? 1n : 0n);
-                // context.states.at(-1)!.accounts!.set(context.subcontext!.address!, { nonce: 1n, balance: context.subcontext!.value, code: context.subcontext!.returndata });
-                context.gas += context.subcontext!.gas - (context.subcontext!.returndata === null ? 0n : 200n * BigInt(context.subcontext!.returndata.byteLength!));
+                context.gas += context.subcontext!.gas;
                 context.blocked = false;
                 context.pc++;
             }
         } else {
             const [gas, address, value, argsOffset, argsSize, retOffset, retSize] = peek(context.stack, 7);
-            // const pGas = context.gas;
 
             // going to have to make a lot more opcodes work like this
             // where if the size is zero, we don't count it at all
@@ -70,7 +68,6 @@ export default {
                     retSize! == 0n ? 0 : Number(retOffset!) + Number(retSize)
                 )
             );
-            // const C_memexpand = context.gas - pGas;
 
             const addressHex = `0x${numberToHex(address!, 40)}`;
 
@@ -81,74 +78,13 @@ export default {
                  || (originalAccount.code === null && originalAccount.nonce == 0n && originalAccount.balance == 0n) )
                 && value != 0n
             ) C_new = 25000n;
-            // console.log({ addressHex, originalAccount });
-
-            // value transfer logic
-            let notEnoughValue = false as boolean;
-            if (value! != 0n) {
-            
-                let account = getStateValue(context.states, null, "accounts", [addressHex]);
-
-                let accountIsCurrent = false as boolean;
-                if (
-                    context.states.at(-1)!.accounts !== null
-                    && context.states.at(-1)!.accounts!.has(addressHex) !== false
-                ) accountIsCurrent = true;
-
-                let contextAccount = getStateValue(context.states, null, "accounts", [context.address!]);
-
-                let contextAccountIsCurrent = false as boolean;
-                if (
-                    context.states.at(-1)!.accounts !== null
-                    && context.states.at(-1)!.accounts!.has(context.address!) !== false
-                ) contextAccountIsCurrent = true;
-
-                if (contextAccount!.balance >= value!) {
-
-                    if (!accountIsCurrent || !contextAccountIsCurrent)
-                        if (context.states.at(-1)!.accounts === null)
-                            context.states.at(-1)!.accounts = new Map();
-
-                    let currentAccount:null|MapComponents<typeof context.states[number]["accounts"]>["value"] = account;
-                    if (!accountIsCurrent) {
-                        if (account !== null) {
-                            currentAccount = {
-                                code: account.code,
-                                nonce: account.nonce,
-                                balance: account.balance + value!
-                            };
-                        } else {
-                            currentAccount = { code: null, nonce: 0n, balance: value! };
-                        }
-                        context.states.at(-1)!.accounts!.set(addressHex, currentAccount);
-                    } else {
-                        account!.balance += value!;
-                    }
-
-                    let currentContextAccount:null|MapComponents<typeof context.states[number]["accounts"]>["value"] = contextAccount;
-                    if (!contextAccountIsCurrent) {
-                        currentContextAccount = {
-                            code: contextAccount!.code,
-                            nonce: contextAccount!.nonce,
-                            balance: contextAccount!.balance -= value!
-                        }
-                        context.states.at(-1)!.accounts!.set(context.address!, currentContextAccount);
-                    } else {
-                        contextAccount!.balance -= value!;
-                    }
-
-                } else {
-                    notEnoughValue = true;
-                }
-
-            }
 
             // i don't think i get the call stipend thing at all
             // what does it mean "subtracted from G_callvalue for z non-zero value transfer"?
             // wouldn't G_callvalue then never be used?
             // i'm probably missing something here
             //
-            // it seems that we don't subtract from G_callvalue?
+            // it seems that we don't "subtract from G_callvalue"?
             let C_xfer = 0n;
             if (value != 0n) C_xfer = 9000n;
 
@@ -173,8 +109,6 @@ export default {
             context.gas -= C_call;
             context.L = C_callgas;
 
-            // console.log({ L: context.L, gas: context.gas, gasArg: gas, C_call, C_gascap, C_extra, C_aaccess, C_xfer, C_new, C_memexpand, pGas, value, originalAccount });
-
             const account = getStateValue(context.states, null, "accounts", [addressHex]);
             const code = account === null ? null : account.code;
             if (code === null) {
@@ -183,29 +117,36 @@ export default {
                 context.subcontext = createContext({
                     origin: context.origin,
                     sender: context.address!,
+                    to: addressHex,
+                    calldata: argsSize! === 0n ? null : context.memory!.slice(Number(argsOffset), Number(argsOffset) + Number(argsSize)),
                     address: addressHex,
                     gas: C_callgas,
+                    value: value!,
                     states: context.states,
                     accountAccessSet: context.accountAccessSet,
                     storageAccessSet: context.storageAccessSet,
                     depth: context.depth + 1,
                     code,
-                    reverted: notEnoughValue ? true : false
+                    // reverted: notEnoughValue ? true : false,
+                    refund: context.refund
                 });
             } else {
                 context.subcontext = createContext({
                     origin: context.origin,
                     sender: context.address!,
+                    to: addressHex,
+                    calldata: argsSize! === 0n ? null : context.memory!.slice(Number(argsOffset), Number(argsOffset) + Number(argsSize)),
                     address: addressHex,
                     gas: C_callgas,
+                    value: value!,
                     states: context.states,
                     accountAccessSet: context.accountAccessSet,
                     storageAccessSet: context.storageAccessSet,
                     depth: context.depth + 1,
                     code,
-                    reverted: notEnoughValue ? true : null
+                    // reverted: notEnoughValue ? true : null,
+                    refund: context.refund
                 });
-                context.states.push({ accounts: null, transientStorage: null, storage: null });
                 context.blocked = true;
             }
         }
